9fb70c44a2bb7543bae44374dc9b9101e89cde05
Mingmar sherpa <gb5479461@gmail.com>
2024-07-08 22:27:30 +0545

    register

--- login.php
+++ login.php
@@ -1,43 +1,54 @@
 <?php include_once("includes/header.php"); ?>
-<main>
-<div class="login-form">
-    <h1>Register Form</h1>
-<form action="">
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-    <div class="form-group">
-        <label for="name">NAME</label>
-        <input type="text" name="email" class="form-control" required>
-    </div>
-</form>
+<?php include_once("configs/db_connection.php"); ?>
+<?php
 
-</div>
+if (isset($_POST['btn_login'])) {
+
+    if (isset($_POST['email']) && !empty($_POST['email'])) {
+        $email = $_POST['email'];
+    } else {
+        $error_name = "Email field is required";
+    }
+
+    if (isset($_POST['password']) && !empty($_POST['password'])) {
+        $password = md5($_POST['password']);
+    } else {
+        $error_nam = "password field is required";
+    }
+
+    $status = 1;
+
+    if (
+      
+        isset($email) &&
+        isset($password) &&
+        isset($status)
+    ) {
+        $sql = "INSERT into tbl_users
+                (email,password,status)
+                VALUES('$email','$password',$status)";
+        $connection->query($sql);
+    }
+}
+?>
+
+<main>
+    <div class="container">
+        <div class="login-form">
+            <h1>Login Form</h1>
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" class="form-control" required>
+                </div>
+                <button type="submit" name="btn_login" class="btn btn-primary">Login</button>
+            </form>
+        </div>
+    </div>
 </main>
 
 <?php include_once("includes/footer.php"); ?>
